2b64faff6cf746ce54fd138604859d33ed6d4c3f
ultrabear <bearodark@gmail.com>
2023-08-11 01:33:45 -0700

    add double ended pressure based buffer allocation

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,74 @@
 #![warn(clippy::pedantic)]
 #![feature(portable_simd)]
 
-use std::sync::mpsc::{channel, Receiver, Sender, TryRecvError};
+use crossbeam_channel::{unbounded as channel, Receiver, Sender, TryRecvError};
 
-use mandelbrot_mt_f64::{generate_buffer, insert_frame_counter, Config};
+use mandelbrot_mt_f64::{generate_buffer, insert_frame_counter, Config, WindowDimensions};
 use minifb::{Key, Window, WindowOptions};
 use std::error::Error;
+
+struct TwoWayPressureValve {
+    spawned: usize,
+    pressure: i64,
+    recv_ch: Receiver<Vec<u32>>,
+    dims: WindowDimensions,
+}
+
+impl Iterator for TwoWayPressureValve {
+    type Item = Vec<u32>;
+
+    /// dynamically allocates or frees vecs in the pipe depending on pressure
+    fn next(&mut self) -> Option<Vec<u32>> {
+        match self.recv_ch.try_recv() {
+            Ok(buf) => {
+                self.pressure = -i64::try_from(self.recv_ch.len()).unwrap();
+
+                Some(if self.pressure < -2 {
+                    drop(buf);
+                    self.spawned -= 1;
+                    let Ok(buf) = self.recv_ch.recv() else {
+                        return None;
+                    };
+                    buf
+                } else {
+                    buf
+                })
+            }
+            Err(TryRecvError::Disconnected) => None,
+            Err(TryRecvError::Empty) => Some(if self.spawned < 100 {
+                self.spawned += 1;
+                vec![0; self.dims.flat_length()]
+            } else {
+                let Ok(buf) = self.recv_ch.recv() else {
+                    return None;
+                };
+                buf
+            }),
+        }
+    }
+}
 
 fn mandelbrot_generator(conf: Config) -> (Sender<Vec<u32>>, Receiver<Vec<u32>>) {
     let (return_send_ch, recv_ch) = channel();
     let (send_ch, return_recv_ch) = channel();
 
-    for _ in 0..5 {
-        return_send_ch
-            .send(vec![0; conf.dims.flat_length()])
-            .unwrap();
-    }
+    //for _ in 0..5 {
+    //    return_send_ch
+    //        .send(vec![0; conf.dims.flat_length()])
+    //        .unwrap();
+    //}
 
     std::thread::spawn(move || {
         let mut scale = conf.starting_scale;
-        let mut spawned = 5;
-        loop {
-            let mut buf = match recv_ch.try_recv() {
-                Ok(buf) => buf,
-                Err(TryRecvError::Disconnected) => break,
-                Err(TryRecvError::Empty) => {
-                    if spawned < 100 {
-                        spawned += 1;
-                        vec![0; conf.dims.flat_length()]
-                    } else {
-                        let Ok(buf) = recv_ch.recv() else { break };
-                        buf
-                    }
-                }
-            };
 
+        let iter = TwoWayPressureValve {
+            spawned: 0,
+            pressure: 0,
+            recv_ch,
+            dims: conf.dims,
+        };
+
+        for mut buf in iter {
             generate_buffer(scale, &mut buf, conf.dims, conf.offset);
 
             scale *= conf.scaling_factor;
